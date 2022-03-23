0c8f463a0f4b6f5bbccd864669f3b30a45e47314
Onix1001 <caunurs@gmail.com>
2022-03-23 12:37:33 +0700

    commit-1

---
+++ app/src/main/java/com/example/prototype/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.prototype;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
