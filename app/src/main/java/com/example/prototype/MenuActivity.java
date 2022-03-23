a2087435c78a14ad7db93b340e1efe28371edd1d
Onix1001 <caunurs@gmail.com>
2022-03-24 00:10:08 +0700

    update(gambar)

--- app/src/main/java/com/example/prototype/MenuActivity.java
+++ app/src/main/java/com/example/prototype/MenuActivity.java
@@ -1,13 +1,30 @@
 package com.example.prototype;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends Activity {
+
+    ImageView ivMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        ivMenu = findViewById(R.id.iv_Menu);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
